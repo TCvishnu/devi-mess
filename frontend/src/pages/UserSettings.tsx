780c1e2ee4447145f72d46369b365fa9694f1775
Vishnu T C <vishnutc13@gmail.com>
2025-05-17 13:19:38 +0530

    feat: profile gender view only

--- frontend/src/pages/UserSettings.tsx
+++ frontend/src/pages/UserSettings.tsx
@@ -4,7 +4,6 @@ import { Icon } from "@iconify/react/dist/iconify.js";
 
 import Input from "../common/components/form/Input";
 import PhoneNumberField from "../common/components/form/PhoneNumberField";
-import MealTypeButton from "../components/user/MealTypeButton";
 import type { ProfileDataType, ResidentialDataType } from "../types/user";
 import { mealTypes } from "@constants/mealTypes";
 import Button from "../common/components/button/Button";
@@ -12,11 +11,15 @@ import Button from "../common/components/button/Button";
 const UserSettings: FC = () => {
   const [profileData, setProfileData] = useState<ProfileDataType>({
     fullName: "Dummy",
-    gender: "MALE",
+    gender: "FEMALE",
     isVeg: true,
     phoneNumber: "9080706050",
     mealType: "Full",
   });
+
+  const meal = mealTypes.find(
+    (mealType) => mealType.mealType === profileData.mealType
+  );
 
   const [residentialData, setResidentialData] =
     useState<null | ResidentialDataType>({
@@ -43,7 +46,7 @@ const UserSettings: FC = () => {
 
   return (
     <div className="w-full flex flex-col items-center overflow-y-auto pb-4">
-      <h1 className=" text-xl font-semibold text-primary mt-2">Edit Profile</h1>
+      <h1 className=" text-xl font-semibold text-primary mt-2"> Profile</h1>
       <form className="w-full flex flex-col gap-2">
         <Input
           label="Full name"
@@ -53,36 +56,13 @@ const UserSettings: FC = () => {
         />
 
         <div className="flex flex-col gap-1">
-          <span className="opacity-60 font-semibold">Gender</span>
-          <div className="w-full flex justify-between">
-            <button
-              type="button"
-              className={`${
-                profileData.gender === "MALE" ? "border-2 border-primary" : ""
-              } py-3 w-40 rounded-md font-semibold`}
-              onClick={() => handleGenderChange("MALE")}
-            >
-              Male
-            </button>
-            <button
-              type="button"
-              className={`${
-                profileData.gender === "FEMALE" ? "border-2 border-primary" : ""
-              } py-3 w-40 rounded-md font-semibold`}
-              onClick={() => handleGenderChange("FEMALE")}
-            >
-              Female
-            </button>
-          </div>
-        </div>
-        <div className="flex flex-col gap-1">
           <span className="opacity-60 font-semibold">Food Type</span>
           <div className="w-full flex justify-between">
             <button
               type="button"
               className={`${
                 profileData.isVeg ? "border-2 border-primary" : ""
-              } py-3 w-40 rounded-md font-semibold`}
+              } py-2 w-40 rounded-md font-semibold`}
               onClick={() => handleFoodTypeChange(true)}
             >
               Veg
@@ -91,66 +71,91 @@ const UserSettings: FC = () => {
               type="button"
               className={`${
                 !profileData.isVeg ? "border-primary border-2 " : ""
-              } py-3 w-40 rounded-md font-semibold`}
+              } py-2 w-40 rounded-md font-semibold`}
               onClick={() => handleFoodTypeChange(false)}
             >
-              non-Veg
+              Non-Veg
             </button>
           </div>
         </div>
         <Button
-          className="w-full text-white font-semibold mt-6"
+          className="w-full text-white font-semibold mt-6 h-12"
           radiusSize="sm"
         >
-          Save Changes
+          Update
         </Button>
       </form>
 
-      <div className="mt-10 w-full flex flex-col gap-3">
+      <div className="w-full border border-gray-300 my-8" />
+
+      <div className="w-full flex flex-col gap-6">
         <PhoneNumberField
           label="Phone Number"
           name="phoneNumber"
           value={profileData.phoneNumber}
           onChange={handleChange}
         />
-        <Button className="w-full text-white font-semibold" radiusSize="sm">
+        <Button
+          className="w-full text-white font-semibold h-12"
+          radiusSize="sm"
+        >
           Change Phone Number
         </Button>
       </div>
 
-      <span className="mt-12 font-semibold opacity-60 text-lg">
-        Your Meal Plan
-      </span>
-      <div className="mt-4 w-full flex gap-3 items-center justify-between">
-        {mealTypes.map(({ mealType, icon }) => (
-          <MealTypeButton
-            key={mealType}
-            mealType={mealType}
-            icon={icon}
-            selectedMealType={profileData.mealType}
-          />
-        ))}
+      <div className="w-full border border-gray-300 my-8" />
+
+      <div className="w-full grid grid-cols-2 text-primary font-semibold">
+        <div className="col-span-1 flex flex-col items-center gap-2">
+          {/* use toTitleCase util function here after merge */}
+          <span>{profileData.gender === "MALE" ? "Male" : "Female"}</span>
+
+          {profileData.gender === "MALE" ? (
+            <Icon icon="ion:male-sharp" className="size-18" />
+          ) : (
+            <Icon icon="ion:female-sharp" className="size-18" />
+          )}
+        </div>
+        <div className=" col-span-1 flex flex-col items-center gap-2">
+          <span>
+            {profileData.mealType + " "}
+            {profileData.mealType === "Full" ? "Day Meal" : "Only"}
+          </span>
+
+          {meal ? (
+            <Icon icon={meal.icon} className=" size-18" />
+          ) : (
+            // just for type safety. no such case
+            <span>Unknown meal type</span>
+          )}
+        </div>
       </div>
 
       {residentialData && (
-        <div className="mt-4 w-full flex flex-col gap-2 items-center ">
-          <span className="mt-12 font-semibold opacity-60 text-lg">
-            Stay Details
-          </span>
-          <div className="flex gap-1 items-center justify-around w-full text-primary">
-            <div className="flex flex-col items-center">
-              <Icon icon="mingcute:building-2-fill" className="size-16" />
-              <span className=" font-semibold">{residentialData.building}</span>
-            </div>
+        <>
+          <div className="w-full border border-gray-300 my-4" />
 
-            <div className="flex flex-col items-center">
-              <Icon icon="game-icons:stairs" className="size-16" />
-              <span className=" font-semibold ">
-                {residentialData.floor} Floor
-              </span>
+          <div className="w-full flex flex-col gap-2 items-center ">
+            <span className="font-semibold opacity-60 text-lg">
+              Stay Details
+            </span>
+            <div className="grid grid-cols-2 w-full text-primary">
+              <div className="flex flex-col items-center">
+                <Icon icon="mingcute:building-2-fill" className="size-16" />
+                <span className=" font-semibold">
+                  {residentialData.building}
+                </span>
+              </div>
+
+              <div className="flex flex-col items-center">
+                <Icon icon="game-icons:stairs" className="size-16" />
+                <span className=" font-semibold ">
+                  {residentialData.floor} Floor
+                </span>
+              </div>
             </div>
           </div>
-        </div>
+        </>
       )}
     </div>
   );
