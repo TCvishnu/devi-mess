e3c24b299518491ea1dfa78c8b85e19b6a90ec59
Vishnu T C <vishnutc13@gmail.com>
2025-05-27 23:35:27 +0530

    feat: settings ui

--- frontend/src/pages/verified-user/UserSettings.tsx
+++ frontend/src/pages/verified-user/UserSettings.tsx
@@ -1,12 +1,12 @@
 import { useState, type FC, type ChangeEvent } from "react";
-
-import { Icon } from "@iconify/react/dist/iconify.js";
+import { Icon } from "@iconify/react";
 
 import Input from "../../common/components/form/Input";
 import PhoneNumberField from "../../common/components/form/PhoneNumberField";
-import type { ProfileDataType, ResidentialDataType } from "../../types/user";
-import { mealTypes } from "@constants/mealTypes";
 import Button from "../../common/components/button/Button";
+import { mealTypes } from "@constants/mealTypes";
+
+import type { ProfileDataType, ResidentialDataType } from "../../types/user";
 
 const UserSettings: FC = () => {
   const [profileData, setProfileData] = useState<ProfileDataType>({
@@ -17,74 +17,80 @@ const UserSettings: FC = () => {
     mealType: "FULL",
   });
 
-  const meal = mealTypes.find(
-    (mealType) => mealType.mealType === profileData.mealType
-  );
-
-  const [residentialData, setResidentialData] =
-    useState<null | ResidentialDataType>({
-      building: "Rockland Arcade",
-      floor: "Top",
-    });
+  const [residentialData] = useState<ResidentialDataType | null>({
+    building: "Rockland Arcade",
+    floor: "Top",
+  });
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-
-    setProfileData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+    setProfileData((prev) => ({ ...prev, [name]: value }));
   };
 
   const handleFoodTypeChange = (isVeg: boolean) => {
     setProfileData((prev) => ({ ...prev, isVeg }));
   };
 
+  const meal = mealTypes.find(
+    (mealType) => mealType.mealType === profileData.mealType
+  );
+
   return (
-    <div className="w-full flex flex-col items-center overflow-y-auto pb-4">
-      <h1 className=" text-xl font-semibold text-primary mt-2"> Profile</h1>
-      <form className="w-full flex flex-col gap-2">
+    <div className="w-full max-w-2xl mx-auto py-6 space-y-8 px-2">
+      <header className="text-center">
+        <h1 className="text-2xl font-bold text-primary">User Settings</h1>
+        <p className="text-gray-500">Manage your profile and preferences</p>
+      </header>
+
+      <div className="bg-white rounded-2xl space-y-4">
+        <h2 className="text-lg font-semibold text-primary">Profile Info</h2>
         <Input
-          label="Full name"
+          label="Full Name"
           name="fullName"
           value={profileData.fullName}
           onChange={handleChange}
         />
 
-        <div className="flex flex-col gap-1">
-          <span className="opacity-60 font-semibold">Food Type</span>
-          <div className="w-full flex justify-between">
+        <div className="space-y-2">
+          <label className="block font-semibold text-sm text-gray-700">
+            Food Preference
+          </label>
+          <div className="flex gap-4">
             <button
               type="button"
-              className={`${
-                profileData.isVeg ? "border-2 border-primary" : ""
-              } py-2 w-40 rounded-md font-semibold`}
               onClick={() => handleFoodTypeChange(true)}
+              className={`w-full py-2 rounded-lg font-semibold transition-all ${
+                profileData.isVeg
+                  ? "bg-green-200 border-2 border-green-600 text-green-800"
+                  : "border-2 border-white" // to prevent UI moving up and down
+              }`}
             >
               Veg
             </button>
             <button
               type="button"
-              className={`${
-                !profileData.isVeg ? "border-primary border-2 " : ""
-              } py-2 w-40 rounded-md font-semibold`}
               onClick={() => handleFoodTypeChange(false)}
+              className={`w-full py-2 rounded-lg font-semibold transition-all ${
+                !profileData.isVeg
+                  ? "bg-red-100 border-2 border-accent text-accent"
+                  : "border-2 border-white" // to prevent UI moving up and down
+              }`}
             >
               Non-Veg
             </button>
           </div>
         </div>
+
         <Button
-          className="w-full text-white font-semibold mt-6 h-12"
+          className="w-full mt-4 text-white font-semibold h-12"
           radiusSize="sm"
         >
-          Update
+          Save Profile
         </Button>
-      </form>
+      </div>
 
-      <div className="w-full border border-gray-300 my-8" />
-
-      <div className="w-full flex flex-col gap-6">
+      <div className="rounded-2xl space-y-4">
+        <h2 className="text-lg font-semibold text-primary">Contact Info</h2>
         <PhoneNumberField
           label="Phone Number"
           name="phoneNumber"
@@ -99,11 +105,9 @@ const UserSettings: FC = () => {
         </Button>
       </div>
 
-      <div className="w-full border border-gray-300 my-8" />
-
-      <div className="w-full grid grid-cols-2 text-gray-600 font-semibold gap-4 py-4">
-        <div className="col-span-1 flex flex-col items-center gap-1 bg-primary-50 rounded-xl p-3">
-          <span className="text-sm uppercase tracking-wide ">
+      <div className="grid grid-cols-2 gap-4">
+        <div className="bg-primary-50 rounded-xl py-4 flex flex-col items-center">
+          <span className="uppercase text-sm font-medium tracking-wide">
             {profileData.gender}
           </span>
           <Icon
@@ -112,20 +116,20 @@ const UserSettings: FC = () => {
                 ? "ion:male-sharp"
                 : "ion:female-sharp"
             }
-            className="size-20 text-primary"
+            className="text-primary size-20"
           />
         </div>
 
-        <div className="col-span-1 flex flex-col items-center gap-1 bg-primary-50 rounded-xl p-3">
-          <span className="text-sm uppercase tracking-wide ">
+        <div className="bg-primary-50 rounded-xl py-4 flex flex-col items-center">
+          <span className="uppercase text-sm font-medium tracking-wide">
             {profileData.mealType === "FULL"
               ? "Full Day Meal"
               : `${profileData.mealType} Only`}
           </span>
           {meal ? (
-            <Icon icon={meal.icon} className="size-20 text-primary" />
+            <Icon icon={meal.icon} className="text-primary size-20" />
           ) : (
-            <span className="text-xs text-red-400 italic">
+            <span className="text-xs italic text-red-400">
               Unknown meal type
             </span>
           )}
@@ -133,13 +137,12 @@ const UserSettings: FC = () => {
       </div>
 
       {residentialData && (
-        <div className="w-full flex flex-col items-center gap-4">
-          <span className="font-semibold text-lg text-primary tracking-wide">
+        <div className="bg-white rounded-2xl py-6 space-y-6">
+          <h2 className="text-lg font-semibold text-primary text-center">
             Stay Details
-          </span>
-
-          <div className="w-full grid grid-cols-2 gap-4">
-            <div className="flex flex-col items-center py-4 px-2">
+          </h2>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="flex flex-col items-center">
               <Icon
                 icon="mingcute:building-2-fill"
                 className="size-20 text-secondary-700"
@@ -147,20 +150,19 @@ const UserSettings: FC = () => {
               <span className="mt-2 text-sm font-medium text-gray-500">
                 Building
               </span>
-              <span className="font-semibold text-lg text-primary text-center">
+              <span className="text-lg font-semibold text-primary text-center">
                 {residentialData.building}
               </span>
             </div>
-
-            <div className="flex flex-col items-center py-4 px-2 ">
+            <div className="flex flex-col items-center">
               <Icon
                 icon="game-icons:stairs"
                 className="size-20 text-secondary-700"
               />
-              <span className="mt-2 text-sm text-gray-500 font-medium">
+              <span className="mt-2 text-sm font-medium text-gray-500">
                 Floor
               </span>
-              <span className="font-semibold text-lg text-primary">
+              <span className="text-lg font-semibold text-primary">
                 {residentialData.floor} Floor
               </span>
             </div>
