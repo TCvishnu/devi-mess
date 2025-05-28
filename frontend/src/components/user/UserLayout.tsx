2b54a5995f0256cbda0a2aa1d2e9483d2cd065f2
Vishnu T C <vishnutc13@gmail.com>
2025-05-28 17:48:23 +0530

    feat: user-access only their data, load-screen-ui

--- frontend/src/components/user/UserLayout.tsx
+++ frontend/src/components/user/UserLayout.tsx
@@ -1,38 +1,47 @@
-import type { FC } from "react";
-
-import { Navigate, Outlet } from "react-router-dom";
+import { FC, useEffect } from "react";
+import { Outlet, useParams, useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 import { useAuthContext } from "@contexts/AuthContext";
 
 const UserLayout: FC = () => {
   const { user } = useAuthContext();
+  const { userID } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      // type safety
+      navigate("/");
+      return;
+    }
+    if (user.id !== userID) {
+      // user tries to access some other user content
+      navigate(`/user/${user.id}`);
+      return;
+    }
+    if (!user.hasOnboarded) {
+      navigate("/complete-profile");
+      return;
+    }
+    if (!user.adminVerified) {
+      navigate("/waiting-for-verification");
+      return;
+    }
+  }, [user, userID, navigate]);
+
   return (
     <div className="w-screen h-screen flex flex-col p-4 bg-primary">
-      {user ? (
-        user.hasOnboarded ? (
-          user.adminVerified ? (
-            <>
-              <div className="w-full flex justify-between">
-                <h1 className="text-white text-3xl font-bold">Hi!</h1>
-                <button className="bg-white text-black font-semibold py-2 px-5 text-sm rounded-xs">
-                  Logout
-                </button>
-              </div>
-              <h1 className="text-white text-3xl font-bold">{user?.name}</h1>
-              <NavBar />
-              <div className="bg-white w-full h-full mt-4 rounded-md px-4 flex flex-col items-center overflow-y-auto">
-                <Outlet />
-              </div>
-            </>
-          ) : (
-            <Navigate to="/waiting-for-verification" />
-          )
-        ) : (
-          <Navigate to={`/complete-profile`} />
-        )
-      ) : (
-        <Navigate to="/" />
-      )}
+      <div className="w-full flex justify-between">
+        <h1 className="text-white text-3xl font-bold">Hi!</h1>
+        <button className="bg-white text-black font-semibold py-2 px-5 text-sm rounded-xs">
+          Logout
+        </button>
+      </div>
+      <h1 className="text-white text-3xl font-bold">{user?.name}</h1>
+      <NavBar />
+      <div className="bg-white w-full h-full mt-4 rounded-md px-4 flex flex-col items-center overflow-y-auto">
+        <Outlet />
+      </div>
     </div>
   );
 };
