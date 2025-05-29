1b5532a629929311a619d261eae8541eb1647d4f
Vishnu T C <vishnutc13@gmail.com>
2025-05-29 11:53:59 +0530

    feat: set admin data

--- frontend/src/pages/admin/AdminLayout.tsx
+++ frontend/src/pages/admin/AdminLayout.tsx
@@ -1,12 +1,51 @@
 import type { FC } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import DatePicker from "@components/admin/DatePicker";
 import AdminDashboard from "./AdminDashboard";
 import { DateContextProvider } from "@contexts/DateContext";
+import { useAuthContext } from "@contexts/AuthContext";
+import { fetchCurrentUser } from "@services/userService";
+import { useNavigate } from "react-router-dom";
 
 const AdminLayout: FC = () => {
-  const [fullName] = useState<string>("Santhosh Kumar");
+  const { user, login: updateUser } = useAuthContext();
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  const getCurrentUser = async () => {
+    try {
+      if (user) return;
+      const { data, error } = await fetchCurrentUser();
+
+      if (!error && data) {
+        updateUser(data);
+      } else {
+        navigate("/");
+      }
+    } catch (err) {
+      console.log(err);
+      navigate("/");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="w-screen h-screen flex items-center justify-center bg-white">
+        <div
+          className="size-32 border-4 border-primary border-t-transparent
+          rounded-full animate-spin drop-shadow-lg"
+        />
+      </div>
+    );
+  }
+
   return (
     <div className="w-screen h-screen flex flex-col p-4 bg-primary overflow-y-auto">
       <div className="w-full flex justify-between">
@@ -16,11 +55,13 @@ const AdminLayout: FC = () => {
         </button>
       </div>
 
-      <h1 className="text-white text-3xl font-bold">{fullName}</h1>
-      <DateContextProvider>
-        <DatePicker />
-        <AdminDashboard />
-      </DateContextProvider>
+      <h1 className="text-white text-3xl font-bold">{user?.name}</h1>
+      {user && (
+        <DateContextProvider>
+          <DatePicker />
+          <AdminDashboard />
+        </DateContextProvider>
+      )}
     </div>
   );
 };
