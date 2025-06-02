4182d87c9fc5d931b8ae816d38c99bf2302fcbe2
Vishnu T C <vishnutc13@gmail.com>
2025-06-02 21:16:43 +0530

    fix: slightly bigger font

--- frontend/src/components/admin/FoodCountDisplay.tsx
+++ frontend/src/components/admin/FoodCountDisplay.tsx
@@ -60,7 +60,7 @@ const FoodCountDisplay: FC = () => {
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-6">
       <div className="rounded-sm shadow-sm p-4 border border-gray-400 active:animate-shake">
         <h3 className="text-lg font-semibold text-primary">Morning</h3>
-        <p className="mt-2 text-3xl font-bold text-accent">
+        <p className="mt-2 text-5xl font-bold text-accent">
           {foodCounts.MORNING.count}
         </p>
         <p className="text-sm text-gray-400 font-medium mt-1">Total Meals</p>
@@ -68,11 +68,11 @@ const FoodCountDisplay: FC = () => {
 
       <div className="rounded-sm shadow-sm p-4 border border-gray-400 active:animate-shake">
         <h3 className="text-lg font-semibold text-primary">Afternoon</h3>
-        <p className="mt-2 text-3xl font-bold text-accent">
+        <p className="mt-2 text-5xl font-bold text-accent">
           {foodCounts.AFTERNOON.count}
         </p>
         <p className="text-sm text-gray-400 font-medium mt-1">Total Meals</p>
-        <div className="mt-3 flex justify-between text-sm text-primary font-semibold">
+        <div className="mt-3 flex justify-between text-primary font-semibold">
           <span>Veg: {foodCounts.AFTERNOON.veg}</span>
           <span>Non-Veg: {foodCounts.AFTERNOON.nonVeg}</span>
         </div>
@@ -80,11 +80,11 @@ const FoodCountDisplay: FC = () => {
 
       <div className="rounded-sm shadow-sm p-4 border border-gray-400 active:animate-shake">
         <h3 className="text-lg font-semibold text-primary">Night</h3>
-        <p className="mt-2 text-3xl font-bold text-accent">
+        <p className="mt-2 text-5xl font-bold text-accent">
           {foodCounts.EVENING.count}
         </p>
         <p className="text-sm text-gray-400 font-medium mt-1">Total Meals</p>
-        <div className="mt-3 flex justify-between text-sm text-primary font-semibold">
+        <div className="mt-3 flex justify-between text-primary font-semibold">
           <span>Veg: {foodCounts.EVENING.veg}</span>
           <span>Non-Veg: {foodCounts.EVENING.nonVeg}</span>
         </div>
