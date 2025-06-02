b6287968edd448df9114f33d6de3509666423322
AKASH-KUMAR-R <akashkumar77464@gmail.com>
2025-06-03 00:54:58 +0530

    feat: report generation integrated

--- frontend/src/components/admin/UserReport.tsx
+++ frontend/src/components/admin/UserReport.tsx
@@ -1,42 +1,139 @@
-import { FC } from "react";
-import dayjs from "dayjs";
+import { FC, useState } from "react"
+import dayjs from "dayjs"
+import { fetchReport } from "@services/reportService"
+import { ReportType } from "@type/enums"
+import ErrorBox from "@form/ErrorBox"
 
 const UserReport: FC = () => {
-  const today = dayjs();
-  const reportMonths = Array.from({ length: 3 }, (_, i) =>
-    today.subtract(i + 1, "month")
-  );
+	const [pending, setPending] = useState({
+		show: false,
+		id: -1,
+		type: "",
+	})
+	const [errorMessage, setErrorMessage] = useState("")
 
-  return (
-    <div className="w-full grid grid-cols-1 gap-y-4 md:grid-cols-3 md:gap-x-4 mt-6">
-      {reportMonths.map((date) => (
-        <div
-          key={date.format("YYYY-MM")}
-          className="w-full rounded-sm border border-gray-300 shadow-sm col-span-1"
-        >
-          <div className="p-6 flex flex-col gap-4 items-center">
-            <div className="text-center">
-              <h2 className="text-xl font-semibold text-primary tracking-wide">
-                {date.format("MMMM")}
-              </h2>
-              <p className="text-sm font-medium text-gray-400">
-                {date.format("YYYY")}
-              </p>
-            </div>
+	const today = dayjs()
+	const reportMonths = Array.from({ length: 3 }, (_, i) =>
+		today.subtract(i + 1, "month")
+	)
 
-            <div className="flex w-full">
-              <button className="w-1/2 p-3 text-sm font-medium text-primary border-r border-accent">
-                Residents
-              </button>
-              <button className="w-1/2 p-3 text-sm font-medium text-accent">
-                Outsiders
-              </button>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+	const getReport = async (
+		id: number,
+		month: number,
+		year: number,
+		reportType: ReportType
+	) => {
+		try {
+			setPending({
+				show: true,
+				id: id,
+				type: reportType,
+			})
+			const { data, error } = await fetchReport(month, year, reportType)
 
-export default UserReport;
+			console.log(error, data)
+			if (!error && data) {
+				setErrorMessage("")
+				const url = window.URL.createObjectURL(data)
+				const link = document.createElement("a")
+
+				document.body.appendChild(link)
+
+				link.href = url
+
+				link.download = "student-report"
+				link.click()
+				window.URL.revokeObjectURL(data)
+				document.body.removeChild(link)
+			} else {
+				setErrorMessage(error || "")
+			}
+		} catch (err: unknown) {
+			console.log(
+				err instanceof Error
+					? err.message
+					: "Error in report generation"
+			)
+			setErrorMessage("Failed to generate report")
+		} finally {
+			setPending({
+				show: false,
+				id: -1,
+				type: "",
+			})
+		}
+	}
+
+	return (
+		<div>
+			{errorMessage && (
+				<div className=" text-red-400 text-center font-semibold mt-4">
+					{errorMessage}
+				</div>
+			)}
+			<div className="w-full grid grid-cols-1 gap-y-4 md:grid-cols-3 md:gap-x-4 mt-2">
+				{reportMonths.map((date, id) => (
+					<div
+						key={date.format("YYYY-MM")}
+						className="w-full rounded-sm border border-gray-300 shadow-sm col-span-1"
+					>
+						<div className="p-6 flex flex-col gap-4 items-center">
+							<div className="text-center">
+								<h2 className="text-xl font-semibold text-primary tracking-wide">
+									{date.format("MMMM")}
+								</h2>
+								<p className="text-sm font-medium text-gray-400">
+									{date.format("YYYY")}
+								</p>
+							</div>
+
+							<div className="flex w-full">
+								<button
+									className="w-1/2 p-3 flex justify-center items-center text-sm font-medium text-primary border-r border-accent"
+									onClick={() =>
+										getReport(
+											id,
+											date.month(),
+											date.year(),
+											ReportType.RESIDENT
+										)
+									}
+									disabled={pending.show}
+								>
+									{/* <div className=" w-4 h-4 border-2 border-b-transparent rounded-full animate-spin " /> */}
+									{pending.id == id &&
+									pending.type === ReportType.RESIDENT ? (
+										<div className=" w-4 h-4 border-2 border-b-transparent rounded-full animate-spin " />
+									) : (
+										"Residents"
+									)}
+								</button>
+								<button
+									className="w-1/2 p-3 flex justify-center items-center text-sm font-medium text-accent"
+									onClick={() =>
+										getReport(
+											id,
+											date.month(),
+											date.year(),
+											ReportType.MESS
+										)
+									}
+									disabled={pending.show}
+								>
+									{pending.id == id &&
+									pending.type === ReportType.MESS ? (
+										<div className=" w-4 h-4 border-2 border-b-transparent rounded-full animate-spin " />
+									) : (
+										"Outsiders"
+									)}
+								</button>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	)
+}
+
+export default UserReport
--- frontend/src/types/enums.ts
+++ frontend/src/types/enums.ts
@@ -1,39 +1,44 @@
 export enum Gender {
-  Male = "MALE",
-  Female = "FEMALE",
+	Male = "MALE",
+	Female = "FEMALE",
 }
 
 export enum MealType {
-  Morning = "MORNING_MEAL",
-  Afternoon = "AFTERNOON_MEAL",
-  Evening = "EVENING_MEAL",
-  Full = "FULL_MEAL",
+	Morning = "MORNING_MEAL",
+	Afternoon = "AFTERNOON_MEAL",
+	Evening = "EVENING_MEAL",
+	Full = "FULL_MEAL",
 }
 
-export type RateMealType = Exclude<MealType, MealType.Full>;
+export type RateMealType = Exclude<MealType, MealType.Full>
 
 export enum UserRole {
-  Admin = "ADMIN",
-  Resident = "RESIDENT",
-  Mess = "MESS",
+	Admin = "ADMIN",
+	Resident = "RESIDENT",
+	Mess = "MESS",
 }
 
 export enum Building {
-  DEVI_HOUSE = "DEVI_HOUSE",
-  ROCKLAND_ARCADE = "ROCKLAND_ARCADE",
+	DEVI_HOUSE = "DEVI_HOUSE",
+	ROCKLAND_ARCADE = "ROCKLAND_ARCADE",
 }
 
 export enum Floor {
-  TOP = "TOP",
-  GROUND = "GROUND",
+	TOP = "TOP",
+	GROUND = "GROUND",
 }
 
 export enum BillType {
-  RENT = "RENT",
-  ELECTRICITY = "ELECTRICITY",
-  WIFI = "WIFI",
-  MORNING_MEAL = "MORNING_MEAL",
-  AFTERNOON_MEAL = "AFTERNOON_MEAL",
-  EVENING_MEAL = "EVENING_MEAL",
-  FULL_MEAL = "FULL_MEAL",
+	RENT = "RENT",
+	ELECTRICITY = "ELECTRICITY",
+	WIFI = "WIFI",
+	MORNING_MEAL = "MORNING_MEAL",
+	AFTERNOON_MEAL = "AFTERNOON_MEAL",
+	EVENING_MEAL = "EVENING_MEAL",
+	FULL_MEAL = "FULL_MEAL",
+}
+
+export enum ReportType {
+	MESS = "MESS",
+	RESIDENT = "RESIDENT",
 }
