f5f8d37749b1dbaedf2c86e32c28a5d06146937e
AKASH-KUMAR-R <akashkumar77464@gmail.com>
2025-06-26 20:47:40 +0530

    fix: auth context import issue solved

--- frontend/src/contexts/AuthProvider.tsx
+++ frontend/src/contexts/AuthProvider.tsx
@@ -1,26 +1,26 @@
-import { useState, ReactNode } from "react";
-import AuthContext from "./authContext";
-import { UserDetails } from "@type/user";
+import { useState, ReactNode } from "react"
+import AuthContext from "./AuthContext"
+import { UserDetails } from "@type/user"
 
 type AuthProviderProps = {
-  children: ReactNode;
-};
+	children: ReactNode
+}
 
 const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [user, setUser] = useState<UserDetails | null>(null);
+	const [user, setUser] = useState<UserDetails | null>(null)
 
-  const login = (newUser: UserDetails) => setUser(newUser);
+	const login = (newUser: UserDetails) => setUser(newUser)
 
-  const updateUser = (updatedUser: Partial<UserDetails>) =>
-    setUser((prev) => ({ ...prev!, ...updatedUser }));
+	const updateUser = (updatedUser: Partial<UserDetails>) =>
+		setUser((prev) => ({ ...prev!, ...updatedUser }))
 
-  const logout = () => setUser(null);
+	const logout = () => setUser(null)
 
-  return (
-    <AuthContext.Provider value={{ user, login, logout, updateUser }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+	return (
+		<AuthContext.Provider value={{ user, login, logout, updateUser }}>
+			{children}
+		</AuthContext.Provider>
+	)
+}
 
-export default AuthProvider;
+export default AuthProvider
